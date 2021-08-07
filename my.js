6fb0e2c5f96f4e8e04db4132b93b5c63dde6691d
samod singh <samod0098@gmail.com>
2021-08-07 13:31:19 +0530

    color-and=font-size

--- my.js
+++ my.js
@@ -1,14 +1,22 @@
- 
-document.write("incrise number" +"<br>")
+document.getElementById("text").style.color="red";
+// document.getElementById("text").style.marginTop = "300px";
+document.getElementById("text").style.font = " bold 100px arial,serif";
+
+
+
+
 document.write("<br>")
 
 
 var s =prompt();
 function add(n){
 
-  n-=-1;
+  n++;
   return n;
 }
 
 s= add(s);
 document.write(s)
+
+
+// document.getElementById("text").style.objectPosition = "0 10%";
