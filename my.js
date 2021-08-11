8fcabfea98ebbadfe19768ed4150bdfe79891989
samod singh <samod0098@gmail.com>
2021-08-11 22:21:03 +0530

    textid-color=answer

--- my.js
+++ my.js
@@ -1,26 +1,25 @@
 document.getElementById("text").style.color="red";
 // document.getElementById("text").style.marginTop = "300px";
 document.getElementById("text").style.font = " bold 100px arial,serif";
-
+document.getElementById("text").style.textAlign="center"
 
 
 
 document.write("<br>")
-var n;
 
 
 var s =prompt();
 function add(n){
-
+  var aswer=document.getElementById("answer").innerHTML=n;
+  document.getElementById("answer").style.font = " bold 100px arial,serif";
+  document.getElementById("answer").style.color = "blue";
+  document.getElementById("answer").style.textAlign = "center";
+  
   n++;
   return n;
 }  
-    var answer=document.getElementById("answer").innerHTML=add(s);
-    document.getElementById("answer").style.font = " bold 100px arial,serif";
-    document.getElementById("answer").style.color = "red";
-    // s=add(s);
+   add(s);
 
-document.write(answer);
 
 
 
